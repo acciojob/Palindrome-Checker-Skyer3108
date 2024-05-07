10c3e6eac45c00827755a93263570f102203494a
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-05-07 15:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // complete the given function
 
 function palindrome(str){
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+	let left=0
+	let right=str.length-1
+
+	while(left<rigth){
+
+		if(cleanStr[left]!==cleanStr[right]){
+
+			return false
+			
+		}
+
+		left++;
+		right--
+	}
+
+	return true
+	
 
 }
 module.exports = palindrome
